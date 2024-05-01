24e21d3980dd01dbb03f369edb7ce180a5e62150
Michael Vogt <michael.vogt@mobi.ch>
2024-05-01 14:03:49 +0200

    styling

--- app/bamon/src/app/power.diagram.component.ts
+++ app/bamon/src/app/power.diagram.component.ts
@@ -26,30 +26,24 @@ export class PowerDiagramComponent {
   chart: any = [];
 
   constructor(private bleBattery: BleBattery) {
-    console.log(this.powerBuffer.toArray().concat([1,100,400,2,44,43,2,1,21]))
-
     effect(() => {
       const data = this.bleBattery.signalData();
       this.powerBuffer.add(data.power);
     });
   }
 
-/*
-  get powerData(): number[] {
-    return
-  }
-*/
   ngAfterViewInit() {
-    const data = this.powerBuffer.toArray().concat([1,100,400,2,44,43,2,1,21]);
+    const data = this.powerBuffer.toArray().concat([1,100,400,2,44,43,2,1,21,333,11,23]);
 
     this.chart = new Chart(this.canvas.nativeElement, {
       type: 'bar',
       data: {
         labels: data.map(()=>'') as unknown[],
         datasets: [{
-          label: 'Power Usage',
           data,
-          borderWidth: 1
+          borderWidth: 0,
+          backgroundColor: 'black',
+          barPercentage: 0.97,
         }]
       },
       options: {
